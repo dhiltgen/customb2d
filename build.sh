6e778beaf00e4c7cf23dee4f8cbac963df7e4fe5
Daniel Hiltgen <daniel.hiltgen@docker.com>
2016-05-23 12:45:18 -0700

    Clean up the notes for multi-binary versions

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-# 1.11 is different
+
+# For 1.11 or greater, there are multiple binaries,
 # https://test.docker.com/builds/Linux/x86_64/docker-1.11.0-rc1.tgz
-# lots of tar files in /usr/local/bin :-(
 
 # Download a specific version you want to play with
 #wget -O docker https://test.docker.com/builds/Linux/x86_64/docker-1.10.0-rc4
-wget -O docker https://s3.amazonaws.com/packages.docker.com-staging/1.10/builds/linux/amd64/docker-1.10.3-cs3
+# wget -O docker https://s3.amazonaws.com/packages.docker.com-staging/1.10/builds/linux/amd64/docker-1.10.3-cs3
 #wget -O docker https://test.docker.com/builds/Linux/x86_64/docker-1.10.2
 
-# Or you can use a local binary with something like this
-#cp ~/bin/docker-1.10.1-cs1-rc2 ./docker
+# Or you can use a local binaries with something like this
+#cp ~/docker-1.11.0/* ./
 
 # Or perhaps a build from a local engine tree to pick up master or your own dev
 #cp ../docker/bundles/latest/binary/docker .
