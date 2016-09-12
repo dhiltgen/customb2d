93776bc7831234e512954af16a74e09a49782edf
Daniel Hiltgen <daniel.hiltgen@docker.com>
2016-09-12 15:39:56 -0700

    Bump for latest 1.11+

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+rm -rf docker
 
 # For 1.11 or greater, there are multiple binaries,
 # https://get.docker.com/builds/Linux/x86_64/docker-1.11.1.tgz
@@ -11,9 +12,9 @@ set -e
 # wget -O docker https://s3.amazonaws.com/packages.docker.com-staging/1.10/builds/linux/amd64/docker-1.10.3-cs3
 #wget -O docker https://test.docker.com/builds/Linux/x86_64/docker-1.10.2
 
-# Or you can use a local binaries with something like this
-#cp ~/bin/1.11/* ./
-cp ~/bin/1.11.1/* ./
+
+
+#tar zxvf ~/Downloads/docker-1.12.1-cs1-rc1.tgz
 
 # Or perhaps a build from a local engine tree to pick up master or your own dev
 #cp ../docker/bundles/latest/binary/docker .
@@ -22,5 +23,5 @@ docker build -t customb2d .
 docker run --rm customb2d > boot2docker.iso
 
 # Copy to the places where machine and friends looks
-cp boot2docker.iso ~/.docker/machine/cache/boot2docker.iso
-cp boot2docker.iso ~/.docker/images/boot2docker.iso
+#cp boot2docker.iso ~/.docker/machine/cache/boot2docker.iso
+#cp boot2docker.iso ~/.docker/images/boot2docker.iso
